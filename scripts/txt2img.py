fa20beccf28c2c8db9f2cce17372eaf77d0a61ed
Wei Yang <platero.yang@gmail.com>
2024-04-17 12:45:05 -0700

    Remove pdb

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -47,7 +47,6 @@ def load_model_from_config(config, ckpt, half_precision=False, verbose=False):
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
-    import pdb; pdb.set_trace()
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
         print("missing keys:")
